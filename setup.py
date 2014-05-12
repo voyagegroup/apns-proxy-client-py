896e48ac6e94c4e55f58b5327ceb263d94bc7119
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-05-12 23:03:53 +0900

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 from setuptools import setup
 
+version = '0.1.0'
+name = 'APNsProxyClient'
+short_description = 'Client library for APNs Proxy Server.'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -11,23 +14,26 @@ requires = [
     ]
 
 setup(
-    name="APNsProxyClient",
-    version="0.1.0",
+    name=name,
+    version=version,
     author='Takashi Nishibayashi',
     author_email="takashi_nishibayashi@voyagegroup.com",
-    description=("Client module for APNs Proxy"),
+    description=short_description,
+    long_description=read('README.rst'),
     license="BSD",
     platforms='any',
     keywords="apns",
     install_requires=requires,
     url='https://github.com/voyagegroup/apns-proxy-client-py',
     packages=['apns_proxy_client', 'tests'],
-    long_description=read('README.md'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
 )
